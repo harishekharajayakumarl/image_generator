174b9ae39255c513e32d6dd0d16ca4734541be12
al harishekhar <alharishekhar@gmail.com>
2025-01-19 15:39:34 +0530

    Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+import Home from "./pages/Home";
+import Generate from "./pages/Generate";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
+
 function App() {
-  return (
-    <div>HELLO</div>
+  return(
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} exact/>
+            <Route path="/post" element={<Generate/>} exact/>
+          </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
