8bea8c3be25012354266e800ec37857a46b79560
al harishekhar <alharishekhar@gmail.com>
2025-01-26 12:38:44 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -1,55 +1,55 @@
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import generateImageRoute from "./routes/GenerateImage.js";
 import posts from "./routes/Posts.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/generateImage/", generateImageRoute);
+app.use("/api/post/", posts);
 
 // error handler
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong!";
-    return res.status(status).json({
-        sucess: false,
-        status,
-        message
-    });
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
-app.use("/api/post/", posts);
-
-// default get
-app.get('/', async (req, res) => {
-    res.status(200).json({
-        message: "hello Devs...",
-    });
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
 });
 
-// connect to db
 const connectDB = () => {
-    mongoose.set("strictQuery", true);
-    mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connected to database"))
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
     .catch((err) => {
-        console.error("Failed to connect to databse");
-        console.error(err);
-    })
-}
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
 
-// Server staring function
 const startServer = async () => {
-    try{
-        connectDB();
-        app.listen(8080, () => console.log("Server started"));
-    } catch(error){
-        console.log(error);
-    }
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
